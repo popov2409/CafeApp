10d7c1c29ebdc8d1680edc3ae25d227ab44d2633
Tim Popov <popov_ta@mail.ru>
2020-10-25 16:40:13 +0300

    Добавлена страница отчета отдельно по ингредиентам и автоматам

--- CafeApp/CafeApp/ViewModels/IngredientCountViewModel.cs
+++ CafeApp/CafeApp/ViewModels/IngredientCountViewModel.cs
@@ -17,13 +17,16 @@ namespace CafeApp.ViewModels
         public ObservableCollection<IngredientCount> IngredientCounts { get; set; }
         public ObservableCollection<IngredientCount> ReportIngredientCounts { get; set; }
 
+        private ObservableCollection<Avtomat> _repAvtomats;
+        public ObservableCollection<Avtomat> ReportAvtomats { get; set; }
+
         private Avtomat _avtomat;
 
         public Command CreateListCommand { get; set; }
 
         public Command AddDataCommand { get; set; }
 
-        public Command CreateIngredientListCommand { get; set; }
+        public Command CreateReportListCommand { get; set; }
 
         /// <summary>
         /// Контсруктор для создания списка ингредиентов при добавлении записи о внесении в автомат
@@ -36,28 +39,6 @@ namespace CafeApp.ViewModels
             this._avtomat = avtomat;
             CreateListCommand =new Command(async ()=>await ExecuteCreateCommand());
             AddDataCommand=new Command(async ()=>await ExecuteAddDataCommand());
-            //MessagingCenter.Subscribe<AddRecordsPage, ObservableCollection<IngredientCount>>(this, "AddRecords", async (obj, records) =>
-            //{
-            //    string userId = Preferences.Get("user_id", "default_value");
-            //    foreach (IngredientCount ingredientCount in records)
-            //    {
-            //        if(ingredientCount.Count==0) continue;
-            //        Record rec=new Record()
-            //        {
-            //            Count = ingredientCount.Count,
-            //            Date = DateTime.Now.ToShortDateString(),
-            //            Id = Guid.NewGuid().ToString(),
-            //            Ingredient = ingredientCount.Ingredient.Id,
-            //            Avtomat = avtomat.Id,
-            //            IsSend = false,
-            //            IsBlock = false,
-            //            User = userId
-            //        };
-            //        await RecordStore.AddItemAsync(rec);
-            //    }
-
-            //    MessagingCenter.Unsubscribe<AddRecordsPage, ObservableCollection<IngredientCount>>(this, "AddRecords");
-            //});
         }
         
         async Task ExecuteAddDataCommand()
@@ -117,16 +98,20 @@ namespace CafeApp.ViewModels
         {
             Title = "Ингредиенты";
             ReportIngredientCounts = new ObservableCollection<IngredientCount>();
-            CreateIngredientListCommand = new Command(async () => await ExecuteCreateIngredientListCommand());
+            ReportAvtomats = new ObservableCollection<Avtomat>();
+            CreateReportListCommand = new Command(async () => await ExecuteReportListCommand());
+            
         }
 
-        async Task ExecuteCreateIngredientListCommand()
+        async Task ExecuteReportListCommand()
         {
             IsBusy = true;
             try
             {
                 ReportIngredientCounts.Clear();
+                ReportAvtomats.Clear();
                 var ingredients = await IngredientStore.GetItemsAsync(true);
+                var avtomats = await AvtomatStore.GetItemsAsync(true);
                 var records = (await RecordStore.GetItemsAsync(true)).Where(c =>
                     DateTime.Parse(c.Date).DayOfYear == DateTime.Now.DayOfYear &&
                     DateTime.Parse(c.Date).Year == DateTime.Now.Year);
@@ -136,13 +121,21 @@ namespace CafeApp.ViewModels
                     item.Count = records.Where(c => c.Ingredient == ingredient.Id).Sum(c => c.Count);
                     ReportIngredientCounts.Add(item);
                 }
+
+                var avtoEnumerable = (await RecordStore.GetItemsAsync(true)).Where(c =>
+                    DateTime.Parse(c.Date).DayOfYear == DateTime.Now.DayOfYear &&
+                    DateTime.Parse(c.Date).Year == DateTime.Now.Year).Select(c => c.Avtomat).Distinct();
+                foreach (string record in avtoEnumerable)
+                {
+                    ReportAvtomats.Add(avtomats.First(c => c.Id == record));
+                }
             }
             catch (Exception ex)
             {
                 Debug.WriteLine(ex);
             }
             finally
-            {
+            { 
                 IsBusy = false;
             }
         }
---
+++ CafeApp/CafeApp/Views/SummaryPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CafeApp.Models;
+using CafeApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CafeApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SummaryPage : ContentPage
+    {
+        private IngredientCountViewModel viewModel;
+
+        public SummaryPage()
+        {
+            InitializeComponent();
+            ModePicker.SelectedIndex = 0;
+            BindingContext = viewModel = new IngredientCountViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (viewModel.ReportAvtomats.Count == 0)
+                viewModel.IsBusy = true;
+            //BindingContext = viewModel = new IngredientCountViewModel();
+            if (viewModel.ReportIngredientCounts.Count == 0)
+                viewModel.IsBusy = true;
+
+        }
+
+        private async void OnItemSelected(object sender, EventArgs e)
+        {
+            var layout = (BindableObject) sender;
+            var ingredient = (IngredientCount) layout.BindingContext;
+            if (ingredient.Count == 0) return;
+            await Navigation.PushAsync(new AvtomatCountPage(new AvtomatCountViewModel(ingredient.Ingredient)));
+        }
+
+        private void SelectedMode_Change(object sender, EventArgs e)
+        {
+            switch (ModePicker.SelectedIndex)
+            {
+                case 0:
+                {
+                    IngredientsCollectionView.IsVisible = false;
+                    AvtomatsCollectionView.IsVisible = true;
+                    break;
+                }
+                case 1:
+                {
+                    IngredientsCollectionView.IsVisible = true;
+                    AvtomatsCollectionView.IsVisible = false;
+                    break;
+                }
+            }
+        }
+    }
+}
